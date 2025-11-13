682c6ba51edd41f9aede9eee8e179b508446317b
Touiiix <leolelievre2006@gmail.com>
2025-11-13 12:45:24 +0100

    class carte mvp

---
+++ MVP/mvpLeo.py
@@ -0,0 +1,9 @@
+class Carte:
+    def __init__(self, niveau : int = 4, carte_id : int = 0, question : str = "", reponse : str = ""):
+        self.niveau = niveau
+        self.carte_id = carte_id
+        self.question = question
+        self.reponse = reponse
+
+    def changer_niveau(valeur):
+        return ""
