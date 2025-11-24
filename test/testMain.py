7bad0b9ea9ff7937bb4c53eb706fcb60b1f58b45
Wiwi-Stevant <he202476@students.ephec.be>
2025-11-24 12:40:48 +0100

    ajout de l'affichage des carte d'un chapitre et de l'ajout de carte dans un chapitre

--- test/testMain.py
+++ test/testMain.py
@@ -1,7 +1,10 @@
 #from modules.quiz import Quiz
 #from modules.flashCard import FlashCards
-#from modules.chapitre import Chapitres
+from modules.chapitre import Chapitres
 
+confirmation = ["o", "oui", "yes", "y"]
+refus = ["n", "non", "no"]    
+    
 def main():#fonction principale -> choix de l'action
     print("""Bonjour, quel option souhaitez-vous exécuter ?
           1. Lancer le quiz
@@ -38,32 +41,49 @@ def gestion_chapitres():
     chap = input()
 
     if chap == ... : # on regarde si le chapitre existe -- code à compléter quand on aura fini le module chapitre -- !!!!!!!!!!!!!!!!!!!!
-        print(f"""Gestion du chapitre : {chap}
-            1. Ajouter une carte
-            2. Supprimer une carte
-            3. Modifier une carte
-            4. Afficher les cartes
-            5. Retour au menu principal
-            :""")
-        choix = input()
+        chap_charger = chap.Chapitres.changer_cartes()
+        while True:
+            print(f"""Gestion du chapitre : {chap}
+                1. Ajouter une carte
+                2. Supprimer une carte
+                3. Modifier une carte
+                4. Afficher les cartes
+                5. Retour au menu principal
+                :""")
+            choix = input()
 
-        if choix == "1":
-            pass
-        elif choix == "2":
-            pass
-        elif choix == "3":
-            pass
-        elif choix == "4":
-            pass
-        elif choix == "5":
-            main()
+            if choix == "1":
+                question = input("Entrez la question de la carte : ")
+                reponse = input("Entrez la réponse de la carte : ")
+                img = input("Entrez le chemin de l'image associée (laisser vide si aucune) : ")
+                confirm = input(f"""Confirmez-vous la création de cette carte  ? 
+                                question : {question}
+                                réponse : {reponse}
+                                image : {img if img else "Aucune"}
+                                (o/n) : """)
+                if confirm.lower() in confirmation:
+                    chap_charger.cree_cartes(... , question, reponse, img)
+                else:
+                    print("Création de la carte annulée.")
+
+            elif choix == "2":
+                pass
+            elif choix == "3":
+                pass
+            elif choix == "4":
+                chap.Chapitres.__str__()
+                
+            elif choix == "5":
+                break
+
+        main()
 
     else:
         print("voulez-vous créer ce chapitre ? (o/n) :")#on demande si on veut cree le chap car le nom n'existe pas
         reponse = input()
 
-        if reponse.lower() == "o" or reponse.lower() == "oui" or reponse.lower() == "yes" or reponse.lower() == "y":
-            pass
+        if reponse.lower() in confirmation:
+            chap = Chapitres(... , chap)
 
         else:
             gestion_chapitres()
