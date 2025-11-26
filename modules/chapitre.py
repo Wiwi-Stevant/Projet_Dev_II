12f27d93805a7569f663e7c02ad0f3b2f9594b28
Wiwi-Stevant <he202476@students.ephec.be>
2025-11-26 15:23:35 +0100

    midification dans le module chapitre (ajout de charger_ carte et sauvgarde_carte)

--- modules/chapitre.py
+++ modules/chapitre.py
@@ -1,17 +1,39 @@
 import json
+import os
 from modules.carte import Cartes
 
 class Chapitres:
-    def __init__(self, id, nom):
-        self.id = id
+    id = 1
+    def __init__(self, nom):
+        Chapitres.idGlobal += 1
+        self.id = Chapitres.idGlobal
+        self.cartes = {}
+        self.idCarte = 1
         self.nom = nom
         self.sauvgarde = f"{self.nom}.json".lower() #on met le non du chapitre avec .json pour dire quel fichier gere la sauvgarde du chap
 
     def charger_cartes(self): #on recupere les carte depuis le json
-        pass
+        if os.path.exists(self.sauvgarde):
+            with open(self.sauvgarde, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                cartes = []
+                for carte_data in data:
+                    carte = Cartes(
+                        carte_data['id'],
+                        carte_data['question'],
+                        carte_data['reponse'],
+                        carte_data.get('img', "")
+                    )
+                    cartes.append(carte)
+                return cartes
+        else:
+            print("Le Chapitre n'existe pas encore.")
 
-    def sauvegarder_cartes(self): #on sauvgarde les cartes dans le json
-        pass
+    def sauvegarder_cartes(self): #on sauvgarde les cartes dans le json -----> metode fait avec AI
+        data_cartes = [carte.to_dict() for carte in self.cartes.values()]
+        with open(self.sauvgarde, 'w', encoding='utf-8') as f:
+            json.dump(data_cartes, f, ensure_ascii=False, indent=4)
+        print(f"chapitre {self.nom} sauvgardé dans {self.sauvgarde}.")
 
     def cree_cartes(self, id, question, reponse, img=""): #on cree une nouvelle carte
         pass
