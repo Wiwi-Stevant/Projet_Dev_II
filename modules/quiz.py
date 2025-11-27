c587f7a3ddbb66e365be6c859a365008de8d622c
Touiiix <leolelievre2006@gmail.com>
2025-11-27 12:46:02 +0100

    Quiz methode tirer_cartes

--- modules/quiz.py
+++ modules/quiz.py
@@ -1,4 +1,15 @@
 from chapitre import Chapitres
-
+import random
+import os
+import json
 class Quiz:
-    pass
+    def __init__(self, chapitre):
+        self.score = 0
+        self.chapitre = chapitre
+
+#charger un chapitre et tirer une carte aléatoire
+    def tirer_cartes(self): 
+        cartes = os.path.relpath(f'../data/{self.chapitre}.json')
+        print(cartes)
+chapitre = Chapitres('test')
+quiz = Quiz(chapitre)
