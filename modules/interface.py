c88de696da29942371fbd25b0804b308c5236faf
Touiiix <leolelievre2006@gmail.com>
2025-12-10 12:42:54 +0100

    IG ajout de texte

--- modules/interface.py
+++ modules/interface.py
@@ -5,11 +5,22 @@ class Interface(Tk):
     def __init__(self):
         super().__init__()
         self.config_fenetre()
+        self.creer_widgets()
+
+#configuration de base de la fenetre
     def config_fenetre(self):
         self.title("Appli de révision")
         self.geometry("1080x720")
         self.minsize(480, 360)
         self.config(background='#5AEBFC')
 
+#ajout du texte, boutons, etc...
+    def creer_widgets(self):
+        self.label = Label(self, text="Application de révision", font=("Helvetica", 40), bg='#5AEBFC', fg='#1749B3')
+        self.label.pack()
+
+#ajout d'évenements de clavier et souris
+    def creer_bindings(self):
+        pass
 appli = Interface()
 appli.mainloop()
