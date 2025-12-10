7f06cd1aeeb9cb9068feee7dc9ece04fc7a4aedb
Touiiix <leolelievre2006@gmail.com>
2025-12-10 12:16:08 +0100

    IG config fenetre

--- modules/interface.py
+++ modules/interface.py
@@ -1,5 +1,15 @@
-import tkinter as tk
-root = tk.Tk()
-root.title("Appli de revision")
+from tkinter import *
 
-root.mainloop()
+#Interface graphique    chatgpt
+class Interface(Tk):
+    def __init__(self):
+        super().__init__()
+        self.config_fenetre()
+    def config_fenetre(self):
+        self.title("Appli de révision")
+        self.geometry("1080x720")
+        self.minsize(480, 360)
+        self.config(background='#5AEBFC')
+
+appli = Interface()
+appli.mainloop()
