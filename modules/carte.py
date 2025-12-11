652f61e9392b83f839133ffbee834feeeca3bd77
Antho-06 <kawalkouskyanthony@gmail.com>
2025-12-11 12:43:08 +0100

    correction ortographe carte

--- modules/carte.py
+++ modules/carte.py
@@ -4,12 +4,12 @@ class Cartes:
         self.reponse = reponse
         self.question = question
         self.img = img
-        self.niveau = niveau # le niveau change le niveau d'apparition de la carte
+        self.niveau = niveau # change le niveau d'apparition de la carte
     
     def get_niveau(self):
         return self.niveau
     
-    def set_niveau(self, niveau): # en fonction de nos reponses dans les quiz et de si on connais ou pas la carte dans les flashcards on modifie le niveau
+    def set_niveau(self, niveau): # en fonction de nos reponses dans les quiz et de si on connait ou pas la carte dans les flashcards on modifie le niveau
         self.niveau = niveau
         if self.niveau < 0:
             self.niveau = 0
@@ -22,7 +22,7 @@ class Cartes:
     def pas_connue(self): # on diminue le niveau de la carte
         self.set_niveau(self.niveau - 1)
 
-    def jsonification(self): # on mais la carte en json pour la sauvgarde
+    def jsonification(self): # on met la carte en json pour la sauvgarde
         return {
             "id": self.id,
             "reponse": self.reponse,
@@ -30,16 +30,6 @@ class Cartes:
             "img": self.img,
             "niveau": self.niveau
         }
-    
-    @staticmethod
-    def cree_carte_depuis_json(data): # on cree une carte a partir d'un json
-        return Cartes(
-            id=data["id"],
-            reponse=data["reponse"],
-            question=data["question"],
-            img=data["img"],
-            niveau=data["niveau"]
-        )
-    
+        
     def __str__(self):
         return f"Carte(id={self.id}, question={self.question}, reponse={self.reponse}, img={self.img}, niveau={self.niveau})"
