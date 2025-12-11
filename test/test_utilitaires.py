82ec0096a9a99fa3b339d948cd02834ef0abe397
Antho-06 <kawalkouskyanthony@gmail.com>
2025-12-11 18:09:33 +0100

    correction ortographe test_utilitaires

--- test/test_utilitaires.py
+++ test/test_utilitaires.py
@@ -1,4 +1,4 @@
 """
-ici on ppourra mettre des tests unitaires (si on a le corrage)
+ici on pourra mettre des tests unitaires (si on a le courage)
 on va utiliser pytest (pour l'installer : pip install pytest)
 """
