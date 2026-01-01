7e2afacfeeeabbc77b13942cdbbc18a52bc3dfd0
Wiwi-Stevant <he202476@students.ephec.be>
2026-01-01 17:38:12 +0100

    lint utilitaire

--- utils/utilitaire.py
+++ utils/utilitaire.py
@@ -1,11 +1,12 @@
 def verification_chapitre(chap, chapitres_dict):
+    """Vérifie si le chapitre existe et contient des cartes."""
     if chap not in chapitres_dict:
         print("Chapitre introuvable.")
         return False
-    
+
     elif not chapitres_dict[chap].cartes:
         print("Le chapitre ne contient aucune carte.")
         return False
-    
+
     else:
         return True
