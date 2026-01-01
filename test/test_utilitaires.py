601acc4b58ceb976f171213fc847ac71d99fb5b5
Wiwi-Stevant <he202476@students.ephec.be>
2026-01-01 17:36:56 +0100

    lint test_unitaires

--- test/test_utilitaires.py
+++ test/test_utilitaires.py
@@ -1,10 +1,4 @@
-"""
-ici on pourra mettre des tests unitaires (si on a le courage)
-on va utiliser pytest (pour l'installer : pip install pytest)
-"""
-
-
-
+"""import pour les test unitaires"""
 import unittest
 from unittest.mock import MagicMock, patch
 from modules.chapitre import Chapitres
@@ -13,6 +7,7 @@ from modules.quiz import Quiz
 
 
 class TestQuiz(unittest.TestCase):
+    """Tests unitaires pour la classe Quiz."""
     def setUp(self):
         self.chapitre = Chapitres("TestUnitaire")
         self.carte1 = Cartes(1, "Capitale de la Belgique ?", "Bruxelles", "")
@@ -22,14 +17,17 @@ class TestQuiz(unittest.TestCase):
         self.quiz = Quiz(self.chapitre)
 
     def test_init(self):
+        """Test de l'initialisation de la classe Quiz."""
         self.assertEqual(self.quiz.score, 0)
         self.assertEqual(self.quiz.chapitre, self.chapitre)
 
     def test_tirer_cartes(self):
+        """Test de la méthode tirer_cartes."""
         carte = self.quiz.tirer_cartes()
         self.assertIn(carte, self.chapitre.cartes.values())
-    
+
     def test_question_ouverte_bon(self):
+        """Test de la gestion d'une question ouverte avec bonne réponse."""
         carte = self.carte1
         reponse_utilisateur = "Bruxelles"
         if reponse_utilisateur.lower() == carte.reponse.lower():
@@ -38,8 +36,9 @@ class TestQuiz(unittest.TestCase):
 
         self.assertEqual(carte.niveau, 5)
         self.assertEqual(self.quiz.score, 1)
-    
+
     def test_question_ouverte_mauvais(self):
+        """Test de la gestion d'une question ouverte avec mauvaise réponse."""
         carte = self.carte1
         reponse_utilisateur = "Paris"
         if reponse_utilisateur.lower() != carte.reponse.lower():
