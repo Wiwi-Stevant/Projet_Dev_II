712369042db2940a7bd987580a6f32f6654745b2
Wiwi-Stevant <he202476@students.ephec.be>
2026-01-01 17:21:31 +0100

    lint Carte + amelioration de l'affichage des carte (__str__())

--- modules/carte.py
+++ modules/carte.py
@@ -1,4 +1,7 @@
 class Cartes:
+    """Classe représentant une carte de flashcard avec question, 
+    réponse, image et niveau de connaissance."""
+
     limiter_niveau = lambda self, n: max(0, min(10, n))
     def __init__(self, id, question, reponse, img ,niveau = 4):
         self.id = id
@@ -6,27 +9,33 @@ class Cartes:
         self.question = question
         self.img = img
         self.niveau = niveau # change le niveau d'apparition de la carte
-    
+
     def get_niveau(self):
+        """Renvoie le niveau de la carte."""
         return self.niveau
-    
-    def set_niveau(self, niveau): # en fonction de nos reponses dans les quiz et de si on connait ou pas la carte dans les flashcards on modifie le niveau
+
+    def set_niveau(self, niveau): 
+        """Définit le niveau de la carte en le limitant entre 0 et 10."""
         self.niveau = self.limiter_niveau(niveau)
         print(self.id, self.niveau) # tuple affichant le niveau actuel de la carte.
 
-    def connue(self): # on augmente le niveau de la carte
+    def connue(self):
+        """On augmente le niveau de la carte."""
         self.set_niveau(self.niveau + 1)
-    
-    def pas_connue(self): # on diminue le niveau de la carte
+
+    def pas_connue(self):
+        """On diminue le niveau de la carte."""
         self.set_niveau(self.niveau - 1)
 
     def jsonification(self): # on met la carte en json pour la sauvgarde
         """ 
         PRE:
-            - La carte possède des attributs valides : id (int), question (str), reponse (str), img (str), niveau (int).
+            - La carte possède des attributs valides : 
+            id (int), question (str), reponse (str), img (str), niveau (int).
 
         POST:
-            - Retourne un dictionnaire contenant toutes les informations de la carte sous forme clé-valeur.
+            - Retourne un dictionnaire contenant toutes 
+            les informations de la carte sous forme clé-valeur.
             - Le dictionnaire est prêt à être sauvegardé ou transmis en JSON.
         """
         return {
@@ -36,6 +45,15 @@ class Cartes:
             "img": self.img,
             "niveau": self.niveau
         }
-        
+
     def __str__(self):
-        return f"Carte(id={self.id}, question={self.question}, reponse={self.reponse}, img={self.img}, niveau={self.niveau})"
+        """Renvoie une représentation textuelle de la carte."""
+
+        retours = f"\n => Carte n°{self.id},"
+        retours += f"\n   -> question : {self.question},"
+        retours += f"\n   -> reponse : {self.reponse},"
+        retours += f"\n   -> img : {self.img if self.img else '/'},"
+        retours += f"\n   -> niveau : {self.niveau}"
+        return retours
+
+    # End-of-file (EOF)
